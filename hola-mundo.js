191e3bdc8aaa8fb656e3cd88c9d0f942668697e8
Sebastianrk18 <20242397@itla.edu.do>
2026-06-02 19:30:58 -0400

    Add files via upload

---
+++ hola-mundo.js
@@ -0,0 +1,4 @@
+// Práctica 4 - Hola Mundo en JavaScript
+console.log("¡Hola Mundo!");
+console.log("Repositorio actualizado en rama main");
+console.log("Notificación enviada a ntfy.sh/devops-itla");
